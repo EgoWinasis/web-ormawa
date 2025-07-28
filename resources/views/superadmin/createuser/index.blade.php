a23bb5d50b736f7871da68e6e2aa5b9ce8254e9a
EgoWinasis <egowinasis22@gmail.com>
2025-07-28 22:10:30 +0700

    create admin

--- resources/views/superadmin/createuser/index.blade.php
+++ resources/views/superadmin/createuser/index.blade.php
@@ -5,7 +5,20 @@
 
     <div class="container" style="padding-top: 3vh">    <div class="row justify-content-center">
         <div class="col-md-8">
-            
+            @if(session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
             <div class="card shadow-sm">
                 <div class="card-header bg-info text-white">
                     <h5 class="mb-0">Tambah Admin Baru</h5>
