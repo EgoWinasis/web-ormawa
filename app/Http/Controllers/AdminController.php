a76d568278ad569a41b9f219902bda3fdb980620
EgoWinasis <egowinasis22@gmail.com>
2025-07-28 22:06:17 +0700

    create admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Notifications\AcceptedNotification;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Carbon;
 class AdminController extends Controller
 {
     public function __construct()
