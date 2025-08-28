1e12ac0af905bcd5767e8c140b027a5d5fb74e8c
Ego Winasis <egowinasis22@gmail.com>
2025-08-28 16:17:47 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -144,7 +144,7 @@ class RegisterController extends Controller
     {
         $response = Http::get('https://api.oase.poltektegal.ac.id/api/web/mahasiswa', [
             'key' => '53jd4f6e-fl0b-4316-8k52-8361khf56a03',
-            'tahun_ajaran' => $request->tahun_angkatan,
+            'tahun_angkatan' => $request->tahun_angkatan,
             'nim' => $request->nim
         ]);
 
