036600b4a4c0262c384677c309270ba81065e862
Ego Winasis <egowinasis22@gmail.com>
2025-08-30 14:12:50 +0700

    responsive arsip

--- resources/views/superadmin/layoutsuper/main.blade.php
+++ resources/views/superadmin/layoutsuper/main.blade.php
@@ -9,9 +9,21 @@
     <meta charset="UTF-8">
     <title>Admin Panel</title>
 
-    <!-- Font Awesome & Bootstrap -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="../../css/root.css">
+
+    <!-- link ke css landing -->
+    <link rel="stylesheet" href="../../css/admin.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- google icons -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>SUPER ADMIN</title>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Custom Style -->
     <style>
