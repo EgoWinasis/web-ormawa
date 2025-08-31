70a52701010e13fb09228428e58c7121dcac8dfb
EgoWinasis <egowinasis22@gmail.com>
2025-08-31 21:19:44 +0700

    main admin

--- resources/views/admin/arsip/create.blade.php
+++ resources/views/admin/arsip/create.blade.php
@@ -1,8 +1,8 @@
 @extends('admin.layoutadmin.main')
 
 @section('konten')
-<div class="container-arsip d-flex active">
-    <div class="container "  style="padding-top:7%">
+<div class="container py-4" style="margin-top: 2rem">
+
     <div class="row justify-content-center" style="padding-bottom: 4%">
         <div class="col-lg-8">
 
@@ -19,67 +19,67 @@
                         <!-- Nama Kegiatan -->
                         <div class="mb-3">
                             <label for="kegiatan-input" class="form-label">Nama Kegiatan</label>
-                            <input type="text" class="form-control @error('nama_kegiatan') is-invalid @enderror" 
-                                   id="kegiatan-input" name="nama_kegiatan" value="{{ old('nama_kegiatan') }}">
+                            <input type="text" class="form-control @error('nama_kegiatan') is-invalid @enderror"
+                                id="kegiatan-input" name="nama_kegiatan" value="{{ old('nama_kegiatan') }}">
                             @error('nama_kegiatan')
-                                <div class="invalid-feedback">*wajib diisi</div>
+                            <div class="invalid-feedback">*wajib diisi</div>
                             @enderror
-                            </div>
-                            <!-- Tanggal Kegiatan -->
-                            <div class="mb-3">
-                                <label for="date-start-input" class="form-label">Tanggal Kegiatan</label>
-                                <input type="date" class="form-control @error('tanggal_mulai') is-invalid @enderror" 
-                                    id="date-start-input" name="tanggal_mulai" value="{{ old('tanggal_mulai') }}">
-                                @error('tanggal_mulai')
-                                    <div class="invalid-feedback">*wajib diisi</div>
-                                @enderror
-                            </div>
+                        </div>
+                        <!-- Tanggal Kegiatan -->
+                        <div class="mb-3">
+                            <label for="date-start-input" class="form-label">Tanggal Kegiatan</label>
+                            <input type="date" class="form-control @error('tanggal_mulai') is-invalid @enderror"
+                                id="date-start-input" name="tanggal_mulai" value="{{ old('tanggal_mulai') }}">
+                            @error('tanggal_mulai')
+                            <div class="invalid-feedback">*wajib diisi</div>
+                            @enderror
+                        </div>
 
-                              <!-- Tempat Kegiatan -->
+                        <!-- Tempat Kegiatan -->
                         <div class="mb-3">
                             <label for="tempat-input" class="form-label">Tempat Kegiatan</label>
-                            <input type="text" class="form-control @error('tempat_kegiatan') is-invalid @enderror" 
-                                   id="tempat-input" name="tempat_kegiatan" value="{{ old('tempat_kegiatan') }}">
+                            <input type="text" class="form-control @error('tempat_kegiatan') is-invalid @enderror"
+                                id="tempat-input" name="tempat_kegiatan" value="{{ old('tempat_kegiatan') }}">
                             @error('tempat_kegiatan')
-                                <div class="invalid-feedback">*wajib diisi</div>
+                            <div class="invalid-feedback">*wajib diisi</div>
                             @enderror
                         </div>
                         <!-- Keterangan -->
                         <div class="mb-3">
                             <label for="keterangan" class="form-label">Keterangan</label>
-                            <input type="text" class="form-control @error('keterangan') is-invalid @enderror" 
+                            <input type="text" class="form-control @error('keterangan') is-invalid @enderror"
                                 id="keterangan" name="keterangan" value="{{ old('keterangan') }}">
                             @error('keterangan')
-                                <div class="invalid-feedback">*wajib diisi</div>
+                            <div class="invalid-feedback">*wajib diisi</div>
                             @enderror
                         </div>
 
                         <!-- Gambar -->
                         <div class="mb-3">
                             <label for="img-input" class="form-label">Gambar</label>
-                            <input type="file" class="form-control @error('gambar') is-invalid @enderror" 
-                                   id="img-input" name="gambar" accept="image/*">
+                            <input type="file" class="form-control @error('gambar') is-invalid @enderror" id="img-input"
+                                name="gambar" accept="image/*">
                             <div class="form-text @error('gambar') text-danger @enderror">
                                 *Pastikan gambar berekstensi JPG atau PNG saja. Max: 2MB
                             </div>
                         </div>
 
-                      
 
-                     
+
+
 
                         <!-- Proposal -->
                         <div class="mb-3">
                             <label for="proposal" class="form-label">Proposal</label>
-                            <input type="file" class="form-control @error('proposal') is-invalid @enderror" 
-                                   id="proposal" name="proposal" 
-                                   accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf">
+                            <input type="file" class="form-control @error('proposal') is-invalid @enderror"
+                                id="proposal" name="proposal"
+                                accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf">
                             <div class="form-text @error('proposal') text-danger @enderror">
                                 *Pastikan file berekstensi PDF saja.
                             </div>
                         </div>
 
-                       
+
 
                         <!-- Buttons -->
                         <div class="d-flex justify-content-between">
@@ -93,7 +93,6 @@
 
         </div>
     </div>
-</div>
 </div>
 
 
