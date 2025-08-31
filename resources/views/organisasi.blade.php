a60f7fce7533f96134d3fdf0c1b67240b728c2e1
EgoWinasis <egowinasis22@gmail.com>
2025-08-31 22:48:22 +0700

    responsive landing

--- resources/views/organisasi.blade.php
+++ resources/views/organisasi.blade.php
@@ -19,44 +19,45 @@
     </div>
 
     <!-- Card Organisasi -->
-    @foreach ($user as $u)
-        @if ($u->nama_organisasi !== 'kesiswaan')
-            <div class="card mb-4 shadow-sm organisasi-card" 
-                 id="card-{{ Str::slug($u->nama_organisasi) }}" 
-                 style="display: none;">
-                <div class="card-body">
-                    <div class="row text-center align-items-center">
-                        
-                        <!-- Kolom Visi -->
-                        <div class="col-md-4 mb-3">
-                            <div class="border rounded p-3 h-100">
-                                <h5 class="fw-bold">Visi</h5>
-                                <p class="text-justify mb-0">{{ $u->visi }}</p>
-                            </div>
-                        </div>
+@foreach ($user as $u)
+@if ($u->nama_organisasi !== 'kesiswaan')
+    <div class="card mb-4 shadow-sm organisasi-card" 
+         id="card-{{ Str::slug($u->nama_organisasi) }}" 
+         style="display: none;">
+        <div class="card-body">
+            <div class="row text-center align-items-start g-4">
 
-                        <!-- Kolom Logo -->
-                        <div class="col-md-4 mb-3">
-                            <img src="storage/{{ $u->foto }}" 
-                                 alt="Logo {{ $u->nama_organisasi }}" 
-                                 class="img-fluid mx-auto d-block" 
-                                 style="max-width: 200px;">
-                            <h5 class="mt-3">{{ $u->nama_organisasi }}</h5>
-                        </div>
+                <!-- Kolom Logo -->
+                <div class="col-md-4">
+                    <img src="storage/{{ $u->foto }}" 
+                         alt="Logo {{ $u->nama_organisasi }}" 
+                         class="img-fluid mx-auto d-block mb-3" 
+                         style="max-width: 200px;">
+                    <h5 class="fw-bold text-uppercase">{{ $u->nama_organisasi }}</h5>
+                </div>
 
-                        <!-- Kolom Misi -->
-                        <div class="col-md-4 mb-3">
-                            <div class="border rounded p-3 h-100">
-                                <h5 class="fw-bold">Misi</h5>
-                                <p class="text-justify mb-0">{{ $u->misi }}</p>
-                            </div>
-                        </div>
-
+                <!-- Kolom Visi -->
+                <div class="col-md-4">
+                    <div class="border rounded p-3 h-100 bg-light">
+                        <h5 class="fw-bold mb-2">Visi</h5>
+                        <p class="text-start mb-0">{{ $u->visi }}</p>
                     </div>
                 </div>
+
+                <!-- Kolom Misi -->
+                <div class="col-md-4">
+                    <div class="border rounded p-3 h-100 bg-light">
+                        <h5 class="fw-bold mb-2">Misi</h5>
+                        <p class="text-start mb-0">{{ $u->misi }}</p>
+                    </div>
+                </div>
+
             </div>
-        @endif
-    @endforeach
+        </div>
+    </div>
+@endif
+@endforeach
+
 </div>
 
 <script>
