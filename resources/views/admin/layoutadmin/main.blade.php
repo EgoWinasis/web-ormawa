70535a24d1571bd70403fe2d6381e0a227b88bed
Ego Winasis <egowinasis22@gmail.com>
2025-09-02 14:33:56 +0700

    notif berhasil

--- resources/views/admin/layoutadmin/main.blade.php
+++ resources/views/admin/layoutadmin/main.blade.php
@@ -26,6 +26,11 @@ $user = Auth::user();
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<!-- Flatpickr CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+<!-- Flatpickr JS -->
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
     <!-- Custom Style -->
     <style>
@@ -422,7 +427,12 @@ $user = Auth::user();
 
 
 
-        
+        flatpickr("#date-start-input", {
+        dateFormat: "Y-m-d",
+        minDate: new Date(new Date().setMonth(new Date().getMonth() - 3)),
+        maxDate: new Date(new Date().setMonth(new Date().getMonth() + 3)),
+        defaultDate: "{{ old('tanggal_mulai') ?? '' }}"
+    });
 
     </script>
 </body>
